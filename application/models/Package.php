57ceb7fcf156c8dbbbbdb0ebb3fc89f36981966d
Crist04 <p.anshuman2001@gmail.com>
2022-07-21 02:42:13 +0530

    first

---
+++ application/models/Package.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Model class for package table
+ */
+class Package extends PS_Model {
+
+	/**
+	 * Constructs the required data
+	 */
+	function __construct() 
+	{
+		parent::__construct( 'bs_packages', 'package_id', 'pkg_' );
+	}
+
+	/**
+	 * Implement the where clause
+	 *
+	 * @param      array  $conds  The conds
+	 */
+	function custom_conds( $conds = array())
+	{ 
+		// default where clause
+		if ( !isset( $conds['no_publish_filter'] )) {
+			$this->db->where( 'status', 1 );
+		}
+
+
+		// package_id condition
+		if ( isset( $conds['package_id'] )) {
+			$this->db->where( 'package_id', $conds['package_id'] );
+		}
+
+		// title condition
+		if ( isset( $conds['title'] )) {
+			$this->db->where( 'title', $conds['title'] );
+		}
+
+        // price condition
+		if ( isset( $conds['price'] )) {
+			$this->db->where( 'price', $conds['price'] );
+		}
+
+        // post_count condition
+		if ( isset( $conds['post_count'] )) {
+			$this->db->where( 'post_count', $conds['post_count'] );
+		}
+
+		// searchterm
+		if ( isset( $conds['searchterm'] )) {
+			$this->db->like( 'title', $conds['searchterm'] );
+		}
+
+		
+		// order_by
+		if ( isset( $conds['order_by_field'] )) {
+
+			$order_by_field = $conds['order_by_field'];
+			$order_by_type = $conds['order_by_type'];
+
+			$this->db->order_by( 'bs_packages.'.$order_by_field, $order_by_type );
+		} else {
+
+
+			$this->db->order_by( 'added_date' );
+		}
+	}
+}
